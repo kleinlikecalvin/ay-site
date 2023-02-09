bc7838a009046d8dbfa8ca02a52a1aae9df7a4b1
streetlightkids <ltj_hq@hotmail.com>
2023-02-08 17:25:46 -0800

    Stands up new site scaffolding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
-import './globals.css'
+import "./globals.css";
+import { Bitter } from "@next/font/google";
+
+const bitter = Bitter({
+  subsets: ["latin"],
+  weight: ["200", "400", "600", "700", "900"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +18,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={bitter.className}>{children}</body>
     </html>
-  )
+  );
 }
