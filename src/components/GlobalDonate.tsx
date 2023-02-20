90843922ab80b88ffdbe5ecbebbb1032fe1cae2d
streetlightkids <ltj_hq@hotmail.com>
2023-02-20 11:01:13 -0800

    Creates GlobalDonate component

---
+++ src/components/GlobalDonate.tsx
@@ -0,0 +1,11 @@
+import "./GlobalDonate.scss";
+import Button from "./Button";
+
+export default function GlobalDonate() {
+  return (
+    <div className="GlobalDonate">
+      <span>Help us with our community graduation party May 28, 2022</span>
+      <Button color="#FFF">Donate Today!</Button>
+    </div>
+  );
+}
