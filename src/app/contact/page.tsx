08b9683c4279ff347e24ec3f3721ff9778660d97
streetlightkids <ltj_hq@hotmail.com>
2023-03-01 13:54:35 -0800

    Stands up Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import "./styles.scss";
+
 export default function Contact() {
-  return <h1>Contact Us</h1>;
+  return (
+    <div className="Contact">
+      <h1>Contact Us</h1>
+      <div className="contact-cards-container">
+        <div className="contact-card">
+          <Image src="" alt="" width="300" height="300" />
+          <div className="copy">
+            <h3>General</h3>
+            <a href="mailto:hello@anaheimyoutopia.com">
+              hello@anaheimyoutopia.com
+            </a>
+          </div>
+        </div>
+        <div className="contact-card">
+          <Image src="" alt="" width="300" height="300" />
+          <div className="copy">
+            <h3>Mailing Address</h3>
+            <p>1105 South Euclid St. #D272 Fullerton, CA 92832</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
