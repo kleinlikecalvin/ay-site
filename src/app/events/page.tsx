56a77bef0ae607049294d1618c6fe10d235f7a95
streetlightkids <ltj_hq@hotmail.com>
2023-04-03 15:31:51 -0700

    Adds classes prop and updates imports

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,12 +1,13 @@
-import "./styles.scss";
+import "./_styles.scss";
 import Event from "@/components/Event";
 import blackSmiley from "@/graphics/blackSmiley.png";
 
 export default function Events() {
-  // Event(classes, landingPage,imageSource, altText, header, blurb)
+  // Event(classes, landingPage, imageSource, altText, header, blurb)
   return (
     <div className="Events">
       <Event
+        classes=""
         landingPage="www.anaheimYoutopia.com/artContest"
         imageSource={blackSmiley}
         altText="black smiley"
