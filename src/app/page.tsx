1144fd39b5d30455c5f4fc66e96f8de6fc4a9c3f
streetlightkids <ltj_hq@hotmail.com>
2023-04-03 15:32:09 -0700

    Updates imports and img tags to Image tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import "./styles.scss";
+import Image from "next/image";
+import "./_styles.scss";
+import landingBanner from "@/graphics/homepageLandingBanner.png";
 import MissionBanner from "@/components/MissionBanner";
 import IntroduceAY from "@/components/IntroduceAY";
 import NeedHelp from "@/components/NeedHelp";
@@ -8,11 +10,12 @@ import NewsletterSignUp from "@/components/NewsletterSignUp";
 export default function Home() {
   return (
     <div className="Home">
-      <div
+      <Image
         className="landing-banner"
-        aria-label={
-          "Anaheim Youtopia full logo graphic with a colorful character jumping with excitement"
-        }
+        src={landingBanner}
+        width={1440}
+        height={400}
+        alt="Anaheim Youtopia full logo graphic with a colorful character jumping with excitement"
       />
       <hr />
       <MissionBanner />
