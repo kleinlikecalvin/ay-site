7a88b9df6ab1a848ccdef9c1f4f31b4f476c8d1a
streetlightkids <ltj_hq@hotmail.com>
2023-04-25 16:01:30 -0700

    Removes landing banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import "./_styles.scss";
-import landingBanner from "@/graphics/homepageLandingBanner.png";
+import Carousel from "@/components/Carousel";
 import MissionBanner from "@/components/MissionBanner";
 import IntroduceAY from "@/components/IntroduceAY";
 import NeedHelp from "@/components/NeedHelp";
@@ -10,14 +9,7 @@ import NewsletterSignUp from "@/components/NewsletterSignUp";
 export default function Home() {
   return (
     <div className="Home">
-      <Image
-        className="landing-banner"
-        src={landingBanner}
-        width={1440}
-        height={400}
-        alt="Anaheim Youtopia full logo graphic with a colorful character jumping with excitement"
-      />
-      <hr />
+      <Carousel />
       <MissionBanner />
       <hr />
       <IntroduceAY />
