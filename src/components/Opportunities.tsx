0dab732ddd9e5c97c97198c38836c0bdda30249a
streetlightkids <ltj_hq@hotmail.com>
2023-07-17 17:06:12 -0700

    Updates Opportunities page to use HTML for dropdown lists

--- src/components/Opportunities.tsx
+++ src/components/Opportunities.tsx
@@ -3,55 +3,50 @@
 import "./_Opportunities.scss";
 
 export default function Opportunities() {
-  function activate(event) {
-    const panel = event.target.nextElementSibling;
-    panel.classList.toggle("activated");
-  }
-
   return (
     <div className="Opportunities">
       <h2>Opportunities</h2>
       <div className="accordions-cont">
-        <button className="accordion" onClick={(event) => activate(event)}>
-          Annual Art Contest
-        </button>
-        <ul className="panel">
-          <li>Sponsor</li>
-          <li>Donor</li>
-        </ul>
-        <button className="accordion" onClick={(event) => activate(event)}>
-          Social Media
-        </button>
-        <ul className="panel">
-          <li>Content Creator</li>
-          <li>Copy Writer</li>
-        </ul>
-        <button className="accordion" onClick={(event) => activate(event)}>
-          Resources
-        </button>
-        <ul className="panel">
-          <li>College Resources Officer</li>
-          <li>Mental Health Resources Officer</li>
-          <li>Legal Resources Officer</li>
-          <li>Health & Wellness Resources Officer</li>
-          <li>Housing Resources Officer</li>
-          <li>Food Resources Officer</li>
-          <li>Financial Resources Officer</li>
-        </ul>
-        <button className="accordion" onClick={(event) => activate(event)}>
-          Fundraising
-        </button>
-        <ul className="panel">
-          <li>Sponsor/Donor</li>
-          <li>Grants Manager</li>
-          <li>Grant Writer</li>
-        </ul>
-        <button className="accordion" onClick={(event) => activate(event)}>
-          Board of Directors
-        </button>
-        <ul className="panel">
-          <li>Advisory Board Member</li>
-        </ul>
+        <details>
+          <summary className="accordion">Annual Art Contest</summary>
+          <ul className="panel">
+            <li>Sponsor</li>
+            <li>Donor</li>
+          </ul>
+        </details>
+        <details>
+          <summary className="accordion">Social Media</summary>
+          <ul className="panel">
+            <li>Content Creator</li>
+            <li>Copy Writer</li>
+          </ul>
+        </details>
+        <details>
+          <summary className="accordion">Resources</summary>
+          <ul className="panel">
+            <li>College Resources Officer</li>
+            <li>Mental Health Resources Officer</li>
+            <li>Legal Resources Officer</li>
+            <li>Health & Wellness Resources Officer</li>
+            <li>Housing Resources Officer</li>
+            <li>Food Resources Officer</li>
+            <li>Financial Resources Officer</li>
+          </ul>
+        </details>
+        <details>
+          <summary className="accordion">Fundraising</summary>
+          <ul className="panel">
+            <li>Sponsor/Donor</li>
+            <li>Grants Manager</li>
+            <li>Grant Writer</li>
+          </ul>
+        </details>
+        <details>
+          <summary className="accordion">Board of Directors</summary>
+          <ul className="panel">
+            <li>Advisory Board Member</li>
+          </ul>
+        </details>
       </div>
     </div>
   );
